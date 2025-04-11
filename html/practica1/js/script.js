5971ea426d806980e456114f21423b5fc56e83b5
PatrykStrek <patryksylwester.strek23@estudiantes.uva.es>
2025-04-11 15:47:25 +0200

    Refactor CSS styles for ordered and unordered lists; add email and phone validation functions in JavaScript

---
+++ html/practica1/js/script.js
@@ -0,0 +1,42 @@
+function esEmailValido(email) {
+    var regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    return regex.test(email);
+}
+
+function esTelefonoValido(telefono) {
+    // Eliminar todos los caracteres que no sean dígitos
+    const digitos = telefono.replace(/\D/g, '');
+    if (digitos.length !== 9) {
+        return false;
+    }
+    
+    // Verificar que solo contenga dígitos, espacios o guiones
+    if (!/^[\d\s-]+$/.test(telefono)) {
+        return false;
+    }
+    
+    return true;
+}
+
+function contarPalabras(texto) {
+    // Eliminar espacios al principio y al final
+    texto = texto.trim();
+    
+    //Si el texto está vacío, no hay palabras
+    if (texto === '') {
+        return 0;
+    }
+    
+    // Dividir el texto por espacios y contar las palabras
+    var palabras = texto.split(/\s+/);
+    return palabras.length;
+}
+
+console.log(contarPalabras("Hola mundo"))
+console.log(contarPalabras("Esto es una prueba") )
+console.log(contarPalabras(" Muchas palabras con espacios "))
+console.log(contarPalabras("") )
+console.log(contarPalabras(" ") ) 
+
+ 
+
